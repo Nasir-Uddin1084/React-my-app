c372127e189cf6104071a80a654f202c50666707
Nasir Uddin Khan <uddin6058@gmail.com>
2021-10-10 00:03:53 +0600

    React my-app setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// add index.css
+// import './index.css';
+
+// JSX = JavaScript XML
+
+const myName = 'Nasir Uddin Khan';
+const fName = 'Abul Khair Khan';
+const mName = 'Nasima Khanom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<div>
+		<h1 className="headStyle anotherClass">Hi Nasir!</h1>
+		<h3>Name: </h3>
+		<p>{myName}</p>
+		<p>Father name: {fName}</p>
+		<p>Mother Name: {mName}</p>
+	</div>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	document.getElementById('root')
+);
