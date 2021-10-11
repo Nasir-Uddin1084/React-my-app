81efa1dd9b5b365140b0c2c26b846bb45273ba7d
Nasir Uddin Khan <uddin6058@gmail.com>
2021-10-11 14:03:57 +0600

    Object Destructing

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,14 @@
 // const mName = 'Nasima Khanom';
 
 function Card(props) {
+	// uses of object Destructing
+	const { title, fName, mName, Address } = props;
 	return (
 		<div className="card">
-			<h3 className="title">{props.title}</h3>
-			<p className="cardTitle2">Father name: {props.fName}</p>
-			<p className="cardTitle3">Mother Name: {props.mName}</p>
-			<p className="cardTitle3">Address: {props.Address}</p>
+			<h3 className="title">{title}</h3>
+			<p className="cardTitle2">Father name: {fName}</p>
+			<p className="cardTitle3">Mother Name: {mName}</p>
+			<p className="cardTitle3">Address: {Address}</p>
 		</div>
 	);
 }
