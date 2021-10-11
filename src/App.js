239aeaeb01d3fe595daa4cd29bf11d331746d8a3
Nasir Uddin Khan <uddin6058@gmail.com>
2021-10-11 14:47:40 +0600

    maping

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import Card from './components/Card';
+import Data from './data.json';
 
 function App() {
+	let myElements = [];
+	/*
+// for loop uses
+for (let x = 0; x < Data.length; x++) {
+		myElements.push(
+			<Card
+				CountryName={Data[x].CountryName}
+				Capital={Data[x].Capital}
+				City={Data[x].City}
+			/>
+		);
+	}
+	*/
+	// maping uses
+	myElements = Data.map((item, nasir) => 
+		<Card key={nasir}
+			CountryName={item.CountryName}
+			Capital={item.Capital}
+			City={item.City}
+		/>);
+
 	return (
 		<div>
 			<h1 className="headStyle">My App!</h1>
-			<Card
-				title="Nasir Uddin"
-				fName="Abul Khair"
-				mName="Nasima Khanom"
-				Address="BD"
-			/>
-			<Card
-				title="Sajjad"
-				fName="Name jani na"
-				mName="Name jani na"
-				Address="BD"
-			/>
-			<Card
-				title="Ansu"
-				fName="Name jani na"
-				mName="Name jani na"
-				Address="BD"
-			/>
+			{myElements}
 		</div>
 	);
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,12 @@
 
 function Card(props) {
 	// uses of object Destructing
-	const { title, fName, mName, Address } = props;
+	const { CountryName, Capital, City } = props;
 	return (
 		<div className="card">
-			<h3 className="title">{title}</h3>
-			<p className="cardTitle2">Father name: {fName}</p>
-			<p className="cardTitle3">Mother Name: {mName}</p>
-			<p className="cardTitle3">Address: {Address}</p>
+			<h3 className="title">{CountryName}</h3>
+			<p className="cardTitle2">{Capital}</p>
+			<p className="cardTitle3">{City}</p>
 		</div>
 	);
 }
